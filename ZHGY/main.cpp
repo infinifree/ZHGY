1b6948b953b4f6a46c293b4caef21a4331b76a14
infinifree <yjqiushui@gmail.com>
2013-12-25 00:17:07 +0800

    Initial Commit

---
+++ ZHGY/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ZHGY
+//
+//  Created by storm on 12/25/13.
+//  Copyright (c) 2013 cloudflyer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
